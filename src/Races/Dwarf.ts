b82557d4b421ecc8c0359966bc5cc000ea0e011e
Jaqueline Santos <jaque.aps@hotmail.com>
2022-10-18 18:42:28 -0300

    feat: Implementando a função createdRacesInstances

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -3,13 +3,21 @@ import Race from './Race';
 export default class Dwarf extends Race {
   name: string;
   dexterity: number;
-  createdRacesInstances: number;
+  _createdRacesInstances: number;
   maxLifePoints: number;
   constructor(name: string, maxLifePoints: number) {
     super(name, maxLifePoints);
     this.name = '';
     this.dexterity = 0;
-    this.createdRacesInstances = 0;
+    this._createdRacesInstances = this.createdRacesInstances;
     this.maxLifePoints = 80;
+  }
+
+  get createdRacesInstances(): number {
+    return this._createdRacesInstances;
+  }
+
+  set createdRacesInstances(value: number) {
+    this._createdRacesInstances = value;
   }
 } 
