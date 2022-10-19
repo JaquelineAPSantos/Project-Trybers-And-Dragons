f32b592f13aebea8a43e69d7938397792c681632
Jaqueline Santos <jaque.aps@hotmail.com>
2022-10-19 16:01:10 -0300

    feat: Implementando na classe Elf a função para receber o valor correto das instâncias

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -3,13 +3,20 @@ import Race from './Race';
 export default class Elf extends Race {
   name: string;
   dexterity: number;
-  createdRacesInstances: number;
+  static racesInstances = 0;
   maxLifePoints: number;
   constructor(name: string, maxLifePoints: number) {
     super(name, maxLifePoints);
     this.name = '';
     this.dexterity = 0;
-    this.createdRacesInstances = 0;
+    Elf.racesInstances += 1;
     this.maxLifePoints = 99;
+  }
+
+  static createdRacesInstances(): number {
+    if (Elf.racesInstances === 0) {
+      throw new Error('Not implemented');
+    }
+    return Elf.racesInstances;
   }
 } 
