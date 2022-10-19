fd666a311afda792c4dd7a3416c38376239878a7
Jaqueline Santos <jaque.aps@hotmail.com>
2022-10-19 15:41:28 -0300

    feat: Implementando na classe Orc a função para receber o valor correto das instâncias

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -3,13 +3,20 @@ import Race from './Race';
 export default class Orc extends Race {
   name: string;
   dexterity: number;
-  createdRacesInstances: number;
+  static racesInstances = 0;
   maxLifePoints: number;
   constructor(name: string, maxLifePoints: number) {
     super(name, maxLifePoints);
     this.name = '';
     this.dexterity = 0;
-    this.createdRacesInstances = 0;
+    Orc.racesInstances += 1;
     this.maxLifePoints = 74;
+  }
+
+  static createdRacesInstances(): number {
+    if (Orc.racesInstances === 0) {
+      throw new Error('Not implemented');
+    }
+    return Orc.racesInstances;
   }
 } 
