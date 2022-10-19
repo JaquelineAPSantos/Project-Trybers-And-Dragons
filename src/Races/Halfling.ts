cb3f012666ffe465f5b46ec9961fc9c9af3b3533
Jaqueline Santos <jaque.aps@hotmail.com>
2022-10-19 15:56:52 -0300

    feat: Implementando na classe Halfling a função para receber o valor correto das instâncias

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -3,13 +3,20 @@ import Race from './Race';
 export default class Halfling extends Race {
   name: string;
   dexterity: number;
-  createdRacesInstances: number;
+  static racesInstances = 0;
   maxLifePoints: number;
-  constructor(name: string, maxLifePoints: number) {
-    super(name, maxLifePoints);
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
     this.name = '';
     this.dexterity = 0;
-    this.createdRacesInstances = 0;
+    Halfling.racesInstances += 1;
     this.maxLifePoints = 60;
+  }
+
+  static createdRacesInstances(): number {
+    if (Halfling.racesInstances === 0) {
+      throw new Error('Not implemented');
+    }
+    return Halfling.racesInstances;
   }
 } 
