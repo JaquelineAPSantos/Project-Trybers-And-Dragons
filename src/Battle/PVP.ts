0e4f4f98fb4cf3f76795fd37dc30db1baf4ed22d
Jaqueline Santos <jaque.aps@hotmail.com>
2022-10-21 15:19:03 -0300

    feat: Conculída a implementação da class PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,22 @@
 import Battle from './Battle';
+import Fighter from '../Fighter';
 
 export default class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
 
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
+    this._player1 = player;
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    while (this._player1.lifePoints > 0
+        && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+    return super.fight();
+  }
 }
